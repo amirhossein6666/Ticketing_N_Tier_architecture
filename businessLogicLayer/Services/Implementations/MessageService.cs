ddd3bc7961e847797c36ab6bfbe3a1cb2819642e
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 11:02:11 +0330

    insert missing members of IMessageService interface in MessageService.cs

--- businessLogicLayer/Services/Implementations/MessageService.cs
+++ businessLogicLayer/Services/Implementations/MessageService.cs
@@ -1,8 +1,28 @@
 using Ticketing.businessLogicLayer.Services.Interfaces;
+using Ticketing.Dtos.MessageDtos;
 
 namespace Ticketing.businessLogicLayer.Services.Implementations;
 
 public class MessageService: IMessageService
 {
 
+    public async Task<MessageReturnDto> CreateMessage(MessageInputDto messageInputDto)
+    {
+    }
+
+    public async Task<MessageDto> GetMessageById(int id)
+    {
+    }
+
+    public async Task<ICollection<MessageDto>> GetMessagesByTicketId(int ticketId)
+    {
+    }
+
+    public async Task<ICollection<MessageDto>> GetMessagesByUserId(int userId)
+    {
+    }
+
+    public async Task<MessageReturnDto> UpdateMessage(int id, UpdateMessageDto updateMessageDto)
+    {
+    }
 }

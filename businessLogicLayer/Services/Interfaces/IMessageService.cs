e1c47836b5d6ba117cd5ba4a5b219ba2c8b8856c
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 10:16:48 +0330

    create message service interface method created

--- businessLogicLayer/Services/Interfaces/IMessageService.cs
+++ businessLogicLayer/Services/Interfaces/IMessageService.cs
@@ -1,6 +1,8 @@
+using Ticketing.Dtos;
+
 namespace Ticketing.businessLogicLayer.Services.Interfaces;
 
 public interface IMessageService
 {
-    
+    public Task<MessageReturnDto> CreateMessage(MessageInputDto messageInputDto);
 }

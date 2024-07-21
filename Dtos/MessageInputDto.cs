d0494ea58fbd0447ba32d61cda9d624d3b4ae4c7
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 10:15:25 +0330

    create MessageInputDto

---
+++ Dtos/MessageInputDto.cs
@@ -0,0 +1,14 @@
+namespace Ticketing.Dtos;
+
+public class MessageInputDto
+{
+    public string Body { get; set; }
+
+    public DateTime SendDate { get; set; }
+
+    public int SenderId { get; set; }
+
+    public int TicketId { get; set; }
+
+    public int? ParentMessageId { get; set; }
+}

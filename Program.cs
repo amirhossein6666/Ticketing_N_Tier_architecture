e1dbc4793c01e0454cd41bc9e640a7aa0db7ba28
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 11:44:04 +0330

    add auto mapper to project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Ticketing.businessLogicLayer.Tools;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//configure mapper
+builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();

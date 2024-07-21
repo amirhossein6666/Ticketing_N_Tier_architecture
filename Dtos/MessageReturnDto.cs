323713cfa28a80cf1e33736487e4a150356de78c
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 10:12:29 +0330

    create MessageReturnDto

---
+++ Dtos/MessageReturnDto.cs
@@ -0,0 +1,17 @@
+namespace Ticketing.Dtos;
+
+public class MessageReturnDto
+{
+    public int Id { get; set; }
+
+    public string Body { get; set; }
+
+    public DateTime SendDate { get; set; }
+
+    public int SenderId { get; set; }
+
+    public int TicketId { get; set; }
+
+    public int? ParentMessageId { get; set; }
+
+}

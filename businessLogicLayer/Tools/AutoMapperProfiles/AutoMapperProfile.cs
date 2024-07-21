4ae36d5e5efd9b60e583a5bbd1881383e012f06a
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 12:36:00 +0330

    CreateMap<Message, MessageReturnDto>();         CreateMap<Message, MessageDto>();

--- businessLogicLayer/Tools/AutoMapperProfiles/AutoMapperProfile.cs
+++ businessLogicLayer/Tools/AutoMapperProfiles/AutoMapperProfile.cs
@@ -9,5 +9,7 @@ public class AutoMapperProfile: Profile
     public AutoMapperProfile()
     {
         CreateMap<MessageInputDto, Message>();
+        CreateMap<Message, MessageReturnDto>();
+        CreateMap<Message, MessageDto>();
     }
 }

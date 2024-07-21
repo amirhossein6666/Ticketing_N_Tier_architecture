4a4fe81aa423542ee58340530872ebb76f22d744
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 12:34:24 +0330

    UpdateMessage method in MessageService.cs implemented

--- businessLogicLayer/Services/Implementations/MessageService.cs
+++ businessLogicLayer/Services/Implementations/MessageService.cs
@@ -56,5 +56,19 @@ public class MessageService: IMessageService
 
     public async Task<MessageReturnDto> UpdateMessage(int id, UpdateMessageDto updateMessageDto)
     {
+        var message = await _messageRepository.GetMessageById(id);
+        if (message is null)
+            throw new NotFoundException($"Message with id {id} not found.");
+        _mapper.Map(updateMessageDto, message);
+        try
+        {
+            var returnedMessage = await _messageRepository.UpdateMessage(message);
+            return _mapper.Map<MessageReturnDto>(returnedMessage);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
     }
 }

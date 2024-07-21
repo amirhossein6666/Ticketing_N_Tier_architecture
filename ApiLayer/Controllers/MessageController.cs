d1db172a1e5aed1794ecf5b144c2ac42f22ecc6e
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-21 12:46:30 +0330

    Message Controller created and craete message method added

---
+++ ApiLayer/Controllers/MessageController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.businessLogicLayer.Services.Interfaces;
+using Ticketing.Dtos.MessageDtos;
+
+namespace Ticketing.ApiLayer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MessageController: ControllerBase
+{
+    private readonly IMessageService _messageService;
+
+    public MessageController(IMessageService messageService)
+    {
+        _messageService = messageService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateMessage(MessageInputDto messageInputDto)
+    {
+        return Ok(await _messageService.CreateMessage(messageInputDto));
+    }
+}

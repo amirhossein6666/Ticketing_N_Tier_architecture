5f76534574624ac489ce74537a48eb57c3f6b17d
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-22 11:16:24 +0330

    UpdateTicket method in repository interface added

--- DataAccessLayer/Interfaces/ITicketRepository.cs
+++ DataAccessLayer/Interfaces/ITicketRepository.cs
@@ -8,4 +8,5 @@ public interface ITicketRepository
     public Task<Ticket> GetTicketById(int id);
     public Task<ICollection<Ticket>> GetAllTickets();
     public Task<ICollection<Ticket>> GetTicketsByCreatorId(int creatorId);
+    public Task<Ticket> UpdateTicket(Ticket updatedTicket);
 }

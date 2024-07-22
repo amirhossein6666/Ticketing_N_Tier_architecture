c0608c8e313269072632cb2520e693046d87809f
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-22 11:03:35 +0330

    create ticket method interface added

--- DataAccessLayer/Interfaces/ITicketRepository.cs
+++ DataAccessLayer/Interfaces/ITicketRepository.cs
@@ -1,6 +1,8 @@
+using Ticketing.DataAccessLayer.Entities;
+
 namespace Ticketing.DataAccessLayer.Interfaces;
 
 public interface ITicketRepository
 {
-    
+    public Task<Ticket> CreateTicket(Ticket ticket);
 }

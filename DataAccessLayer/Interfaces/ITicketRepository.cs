aa89b0beb6f4bd639e280b7077b885814717c83b
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-22 11:07:05 +0330

    GetAllTickets methods interface added

--- DataAccessLayer/Interfaces/ITicketRepository.cs
+++ DataAccessLayer/Interfaces/ITicketRepository.cs
@@ -6,4 +6,5 @@ public interface ITicketRepository
 {
     public Task<Ticket> CreateTicket(Ticket ticket);
     public Task<Ticket> GetTicketById(int id);
+    public Task<ICollection<Ticket>> GetAllTickets();
 }

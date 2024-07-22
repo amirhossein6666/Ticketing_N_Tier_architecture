e7bcaedd84dbda1cc3e1afd775e7783eed9eb01c
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-22 14:23:24 +0330

    change MessageDto.cs for returning only the required properties

--- Dtos/MessageDtos/MessageDto.cs
+++ Dtos/MessageDtos/MessageDto.cs
@@ -4,20 +4,10 @@ namespace Ticketing.Dtos.MessageDtos;
 
 public class MessageDto
 {
-    public int Id { get; set; }
-
     public string Body { get; set; }
-
     public DateTime SendDate { get; set; }
-
-    public int SenderId { get; set; }
-    public User Sender { get; set; }
-
-    public int TicketId { get; set; }
-    public Ticket Ticket { get; set; }
-
-    public int? ParentMessageId { get; set; }
-    public Message ParentMessage { get; set; }
-    public ICollection<Message> Replies { get; set; }
-
+    public string SenderUsername { get; set; }
+    public string ParentMessageBody{ get; set; }
+    public DateTime ParentMessageSendDate{ get; set; }
+    public string ParentMessageSenderUsername{ get; set; }
 }

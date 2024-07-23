9cf9964b8299928732a94c8258838df1f8eaea31
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 13:06:22 +0330

    implement CreateTicket method in TicketService.cs

--- Dtos/ResponseDtos/TicketResponseDtos/CreateUpdateTicketResponseDto.cs
+++ Dtos/ResponseDtos/TicketResponseDtos/CreateUpdateTicketResponseDto.cs
@@ -1,8 +1,9 @@
 using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.TicketDtos;
 
 namespace Ticketing.Dtos.ResponseDtos.TicketResponseDtos;
 
-public class CreateUpdateTicketResponseDto: BaseResponseDto<CreateUpdateTicketResponseDto>
+public class CreateUpdateTicketResponseDto: BaseResponseDto<CreateUpdateTicketDto>
 {
 
 }
--- businessLogicLayer/Services/Implementations/TicketService.cs
+++ businessLogicLayer/Services/Implementations/TicketService.cs
@@ -1,5 +1,8 @@
 using AutoMapper;
 using Ticketing.businessLogicLayer.Services.Interfaces;
+using Ticketing.DataAccessLayer.Entities;
+using Ticketing.DataAccessLayer.Enums;
+using Ticketing.DataAccessLayer.Interfaces;
 using Ticketing.Dtos.ResponseDtos.TicketResponseDtos;
 using Ticketing.Dtos.TicketDtos;
 
@@ -7,17 +10,39 @@ namespace Ticketing.businessLogicLayer.Services.Implementations;
 
 public class TicketService: ITicketService
 {
-    private readonly ITicketService _ticketService;
+    private readonly ITicketRepository _ticketRepository;
     private readonly IMapper _mapper;
 
-    public TicketService(ITicketService ticketService, IMapper mapper)
+    public TicketService(ITicketRepository ticketRepository, IMapper mapper)
     {
-        _ticketService = ticketService;
+        _ticketRepository = ticketRepository;
         _mapper = mapper;
     }
 
     public async Task<CreateUpdateTicketResponseDto> CreateTicket(TicketInputDto ticketInputDto)
     {
+        var ticket = _mapper.Map<Ticket>(ticketInputDto);
+        ticket.Status = Status.Unread;
+        try
+        {
+            var returnedTicket = await _ticketRepository.CreateTicket(ticket);
+            return new CreateUpdateTicketResponseDto()
+            {
+                IsSuccess = true,
+                StatusCode = StatusCodes.Status201Created,
+                Message = $"ticket with id {returnedTicket.Id} created",
+                Data = _mapper.Map<CreateUpdateTicketDto>(returnedTicket)
+            };
+        }
+        catch (Exception e)
+        {
+            return new CreateUpdateTicketResponseDto()
+            {
+                IsSuccess = false,
+                StatusCode = StatusCodes.Status400BadRequest,
+                Message = e.ToString()
+            };
+        }
 
     }
 }

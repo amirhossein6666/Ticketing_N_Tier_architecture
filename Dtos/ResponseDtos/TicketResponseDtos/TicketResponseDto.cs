9d73b314dee7dddb8f687ae2adc923f3a9297a3b
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 13:51:54 +0330

    TicketResponseDto created

---
+++ Dtos/ResponseDtos/TicketResponseDtos/TicketResponseDto.cs
@@ -0,0 +1,9 @@
+using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.TicketDtos;
+
+namespace Ticketing.Dtos.ResponseDtos.TicketResponseDtos;
+
+public class TicketResponseDto: BaseResponseDto<TicketDto>
+{
+
+}

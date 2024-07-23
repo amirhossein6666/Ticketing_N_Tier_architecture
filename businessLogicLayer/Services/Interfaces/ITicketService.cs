cbe70442fb05104c96c8fdc019c9a237b2fea82c
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 12:24:50 +0330

    add createTicket method to ticket service interface

--- businessLogicLayer/Services/Interfaces/ITicketService.cs
+++ businessLogicLayer/Services/Interfaces/ITicketService.cs
@@ -1,6 +1,9 @@
+using Ticketing.Dtos.ResponseDtos.TicketResponseDtos;
+using Ticketing.Dtos.TicketDtos;
+
 namespace Ticketing.businessLogicLayer.Services.Interfaces;
 
 public interface ITicketService
 {
-    public Task<>
+    public Task<CreateUpdateTicketResponseDto> CreateTicket(TicketInputDto ticketInputDto);
 }

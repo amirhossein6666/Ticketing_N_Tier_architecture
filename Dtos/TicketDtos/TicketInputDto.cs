e3c8c451618f842a77a4f8642625382833cf3842
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 12:41:35 +0330

    add desired properties to TicketInputDto.cs

--- Dtos/TicketDtos/TicketInputDto.cs
+++ Dtos/TicketDtos/TicketInputDto.cs
@@ -2,5 +2,10 @@ namespace Ticketing.Dtos.TicketDtos;
 
 public class TicketInputDto
 {
-    
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public int CreatorId { get; set; }
+
 }

dccb1804d36a782c7009f74429e4a09c87387c93
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 13:27:54 +0330

    remvoe undesired Id property from TicketInputDto.cs

--- Dtos/TicketDtos/TicketInputDto.cs
+++ Dtos/TicketDtos/TicketInputDto.cs
@@ -2,8 +2,6 @@ namespace Ticketing.Dtos.TicketDtos;
 
 public class TicketInputDto
 {
-    public int Id { get; set; }
-
     public string Title { get; set; }
 
     public int CreatorId { get; set; }

081e4227a869c49a0c9633b9914b74cbf9e2c7d8
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 15:02:27 +0330

    Create map between ticket and ticketDto and ticketListDto and other maps between ticket related Dtos and models

--- businessLogicLayer/Tools/AutoMapperProfiles/AutoMapperProfile.cs
+++ businessLogicLayer/Tools/AutoMapperProfiles/AutoMapperProfile.cs
@@ -20,5 +20,11 @@ public class AutoMapperProfile: Profile
 
         CreateMap<TicketInputDto, Ticket>();
         CreateMap<Ticket, CreateUpdateTicketDto>();
+        CreateMap<UpdateTicketInputDto, Ticket>();
+        CreateMap<Ticket, TicketListDto>();
+        CreateMap<Ticket, TicketDto>()
+            .ForMember(dest => dest.CreatorUsername, opt => opt.MapFrom(src => src.Creator.Username))
+            .ForMember(dest => dest.Messages, opt => opt.MapFrom(src => src.Messages));
+
     }
 }

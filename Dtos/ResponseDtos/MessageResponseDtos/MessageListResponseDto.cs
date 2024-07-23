af4045e70e5fb237c0a4db5ec8b553c0cee84b0b
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 09:48:05 +0330

    MessageListResponseDto created for methods that return a list of messagesDtos

---
+++ Dtos/ResponseDtos/MessageResponseDtos/MessageListResponseDto.cs
@@ -0,0 +1,9 @@
+using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.MessageDtos;
+
+namespace Ticketing.Dtos.ResponseDtos.MessageResponseDtos;
+
+public class MessageListResponseDto: BaseResponseDto<ICollection<MessageDto>>
+{
+
+}

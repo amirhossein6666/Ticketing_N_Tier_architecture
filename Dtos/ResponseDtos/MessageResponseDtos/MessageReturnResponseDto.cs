f5863f11d6c54005b812469e6499038f04e16766
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 09:08:13 +0330

    MessageReturnResponseDto that is the return type of the create and update method of message service created

---
+++ Dtos/ResponseDtos/MessageResponseDtos/MessageReturnResponseDto.cs
@@ -0,0 +1,9 @@
+using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.MessageDtos;
+
+namespace Ticketing.Dtos.ResponseDtos.MessageResponseDtos;
+
+public class MessageReturnResponseDto: BaseResponseDto<MessageReturnDto>
+{
+
+}

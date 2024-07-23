bab78d251630f61d6c080c41a8ee356facfa1a0a
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 14:21:08 +0330

    GetTicketsByCreatorId and UpdateTicket methods are added to ticketService interface

--- businessLogicLayer/Services/Interfaces/ITicketService.cs
+++ businessLogicLayer/Services/Interfaces/ITicketService.cs
@@ -8,4 +8,6 @@ public interface ITicketService
     public Task<CreateUpdateTicketResponseDto> CreateTicket(TicketInputDto ticketInputDto);
     public Task<TicketResponseDto> GetTicketById(int id);
     public Task<TicketListResponseDto> GetAllTickets();
+    public Task<TicketListResponseDto> GetTicketsByCreatorId(int CreatorId);
+    public Task<CreateUpdateTicketResponseDto> UpdateTicket(int id , UpdateTicketInputDto updateTIcketInputDto)
 }

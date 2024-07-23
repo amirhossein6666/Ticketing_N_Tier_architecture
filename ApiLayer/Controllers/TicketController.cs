9d607c336b98d73e0930efe93819718afbde53d3
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-23 13:27:23 +0330

    create TicketController and add CreateTicket method in it

---
+++ ApiLayer/Controllers/TicketController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Ticketing.businessLogicLayer.Services.Interfaces;
+using Ticketing.Dtos.TicketDtos;
+
+namespace Ticketing.ApiLayer.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class TicketController: ControllerBase
+{
+    private readonly ITicketService _ticketService;
+
+    public TicketController(ITicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateTicket(TicketInputDto ticketInputDto)
+    {
+        return Ok(await _ticketService.CreateTicket(ticketInputDto));
+    }
+}

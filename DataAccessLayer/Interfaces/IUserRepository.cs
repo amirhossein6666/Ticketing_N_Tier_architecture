a82e6e5d6d92b3254a34d259dca279a16f58ced9
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 10:44:50 +0330

    GetUserByUsername method added to user repository interface

--- DataAccessLayer/Interfaces/IUserRepository.cs
+++ DataAccessLayer/Interfaces/IUserRepository.cs
@@ -7,5 +7,6 @@ public interface IUserRepository
 {
     public Task<User> CreateUser(User user);
     public Task<User?> GetUserById(int id);
+    public Task<User?> GetUserByUsername(string username);
     public Task<ICollection<User>> GetUsersByRole(Role role);
 }

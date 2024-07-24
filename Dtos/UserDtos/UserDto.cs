cc747db191589ac310c11ec64b543f854492fab0
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 11:28:07 +0330

    add two property to userDto.Cs

--- Dtos/UserDtos/UserDto.cs
+++ Dtos/UserDtos/UserDto.cs
@@ -1,3 +1,4 @@
+using Ticketing.DataAccessLayer.Entities;
 using Ticketing.DataAccessLayer.Enums;
 
 namespace Ticketing.Dtos.UserDtos;
@@ -7,4 +8,9 @@ public class UserDto
     public string Username { get; set; }
 
     public Role Role { get; set; }
+
+    public ICollection<Ticket> CreatedTickets { get; set; }
+
+    public ICollection<Ticket> AnsweredTicket { get; set; }
+
 }

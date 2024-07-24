dfc9861c96a8ddb48bc3ddd8e5bde5370cf484bd
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 09:29:24 +0330

    Migration added after add IsDeleted property

---
+++ DataAccessLayer/Migrations/20240724055006_LogicalDelete.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketing.Migrations
+{
+    /// <inheritdoc />
+    public partial class LogicalDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Tickets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Messages",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Messages");
+        }
+    }
+}

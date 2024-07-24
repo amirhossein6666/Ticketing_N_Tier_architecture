b0e559e26545b579eaf394ec0d188b675c799d2e
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 10:38:54 +0330

    CreateUser method added to user repository interface

--- DataAccessLayer/Interfaces/IUserRepository.cs
+++ DataAccessLayer/Interfaces/IUserRepository.cs
@@ -1,6 +1,8 @@
+using Ticketing.DataAccessLayer.Entities;
+
 namespace Ticketing.DataAccessLayer.Interfaces;
 
 public interface IUserRepository
 {
-    
+    public Task<User> CreateUser(User user);
 }

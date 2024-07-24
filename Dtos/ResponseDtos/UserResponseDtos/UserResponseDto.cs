4ecd52f03e25a0d263bfd906eb175c3484f8a9b7
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 12:52:59 +0330

    UserResponseDto class created for return type of GetUserByID method and atc

---
+++ Dtos/ResponseDtos/UserResponseDtos/UserResponseDto.cs
@@ -0,0 +1,9 @@
+using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.UserDtos;
+
+namespace Ticketing.Dtos.ResponseDtos.UserResponseDtos;
+
+public class UserResponseDto: BaseResponseDto<UserDto>
+{
+
+}

fbe35b4bb27175a521704104aa7f4f15b83b222f
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-24 13:09:05 +0330

    Add Login method to user service interface

--- businessLogicLayer/Services/Interfaces/IUserService.cs
+++ businessLogicLayer/Services/Interfaces/IUserService.cs
@@ -5,6 +5,7 @@ namespace Ticketing.businessLogicLayer.Services.Interfaces;
 
 public interface IUserService
 {
+    public Task<LoginResponseDto> Login(LoginInputDto loginInputDto);
     public Task<CreateUpdateUserResponseDto> CreateUser(CreateUpdateUserInputDto createUpdateUserInputDto);
     public Task<UserResponseDto> GetUserById(int id);
     public Task<UserResponseDto> GetUserByUsername(string username);

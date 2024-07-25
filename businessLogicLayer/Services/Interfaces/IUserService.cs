ef99dc4c8c00a340a43c4dc75ceeb1caf9762ecf
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-25 13:17:52 +0330

    fix names in IUserService.cs

--- businessLogicLayer/Services/Interfaces/IUserService.cs
+++ businessLogicLayer/Services/Interfaces/IUserService.cs
@@ -7,9 +7,9 @@ namespace Ticketing.businessLogicLayer.Services.Interfaces;
 public interface IUserService
 {
     public Task<LoginResponseDto> Login(LoginInputDto loginInputDto);
-    public Task<CreateUpdateUserResponseDto> CreateUser(CreateUpdateUserInputDto createUpdateUserInputDto);
+    public Task<CreateUpdateUserResponseDto> CreateUser(CreateUserInputDto createUserInputDto);
     public Task<UserResponseDto> GetUserById(int id);
     public Task<UserResponseDto> GetUserByUsername(string username);
     public Task<UserListResponseDto> GetUsersByRole(string  role);
-    public Task<CreateUpdateUserResponseDto> UpdateUser(CreateUpdateUserInputDto createUpdateUserInputDto, int id);
+    public Task<CreateUpdateUserResponseDto> UpdateUser(UpdateUserInputDto updateUserInputDto, int id);
 }

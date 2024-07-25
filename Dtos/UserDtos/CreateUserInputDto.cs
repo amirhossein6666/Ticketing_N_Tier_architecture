2e16534fc0f4a101ed637a0adf8fafe23078fcfe
amirhossein6666 <python.developers.ir@gmail.com>
2024-07-25 13:26:41 +0330

    add properties of CreateUserInputDto.cs and UpdateUserInputDto.cs

--- Dtos/UserDtos/CreateUserInputDto.cs
+++ Dtos/UserDtos/CreateUserInputDto.cs
@@ -1,6 +1,13 @@
+using Ticketing.DataAccessLayer.Enums;
+
 namespace Ticketing.Dtos.UserDtos;
 
 public class CreateUserInputDto
 {
-    
+    public string Username { get; set; }
+
+    public string password { get; set; }
+
+    public Role Role { get; set; }
+
 }
--- Dtos/UserDtos/UpdateUserInputDto.cs
+++ Dtos/UserDtos/UpdateUserInputDto.cs
@@ -2,5 +2,9 @@ namespace Ticketing.Dtos.UserDtos;
 
 public class UpdateUserInputDto
 {
-    
+    public string? Username { get; set; }
+
+    public string? Password { get; set; }
+
+
 }

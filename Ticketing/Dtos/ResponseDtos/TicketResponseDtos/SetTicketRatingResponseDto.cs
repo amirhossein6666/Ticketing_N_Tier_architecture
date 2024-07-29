cca2658be2ab880820cf5ab9f206bf5287d9d76c
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-07-29 10:47:20 +0330

    SetTicketRatingResponseDto the return type of set TicketRating created

---
+++ Ticketing/Dtos/ResponseDtos/TicketResponseDtos/SetTicketRatingResponseDto.cs
@@ -0,0 +1,9 @@
+using Ticketing.Dtos.BaseDtos;
+using Ticketing.Dtos.TicketDtos;
+
+namespace Ticketing.Dtos.ResponseDtos.TicketResponseDtos;
+
+public class SetTicketRatingResponseDto: BaseResponseDto<SetTicketRatingDto>
+{
+
+}

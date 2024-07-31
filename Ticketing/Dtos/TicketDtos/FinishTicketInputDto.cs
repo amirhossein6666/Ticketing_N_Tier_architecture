4ff4bc37f7b9eba06ba12a215d5b9af1e2412b4e
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-07-31 11:01:42 +0330

    little fix

--- Ticketing/Dtos/TicketDtos/FinishTicketInputDto.cs
+++ Ticketing/Dtos/TicketDtos/FinishTicketInputDto.cs
@@ -2,6 +2,6 @@ namespace Ticketing.Dtos.TicketDtos;
 
 public class FinishTicketInputDto
 {
-    public int Submitter { get; set; }
+    public int SubmitterId { get; set; }
     public int TicketId { get; set; }
 }

e858b79e02b6d54ea527a62418a161d3b6471ba9
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-03 11:03:31 +0330

    big change for debugging setRating method for a user

---
+++ Ticketing/DataAccessLayer/Implementations/SupporterRatingRepository.cs
@@ -0,0 +1,23 @@
+using Ticketing.DataAccessLayer.Context;
+using Ticketing.DataAccessLayer.Entities;
+using Ticketing.DataAccessLayer.Interfaces;
+
+namespace Ticketing.DataAccessLayer.Implementations;
+
+public class SupporterRatingRepository: ISupporterRatingRepository
+{
+    private readonly AppDbContext _appDbContext;
+
+    public SupporterRatingRepository(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<SupporterRating> AddRating(SupporterRating supporterRating)
+    {
+        Console.WriteLine("hellooo");
+        _appDbContext.SupporterRatings.Add(supporterRating);
+        await _appDbContext.SaveChangesAsync();
+        return supporterRating;
+    }
+}
---
+++ Ticketing/DataAccessLayer/Migrations/20240803072844_UserRatingRelation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ticketing.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserRatingRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SupporterRatings_Tickets_RelatedTicketId",
+                table: "SupporterRatings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SupporterRatings_Users_RatedUserId",
+                table: "SupporterRatings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SupporterRatings_RelatedTicketId",
+                table: "SupporterRatings");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SupporterRatings_RelatedTicketId",
+                table: "SupporterRatings",
+                column: "RelatedTicketId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SupporterRatings_Tickets_RelatedTicketId",
+                table: "SupporterRatings",
+                column: "RelatedTicketId",
+                principalTable: "Tickets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SupporterRatings_Users_RatedUserId",
+                table: "SupporterRatings",
+                column: "RatedUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

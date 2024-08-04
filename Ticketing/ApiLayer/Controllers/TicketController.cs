e394a46efc1e91176fccb0fb1c8f925bbe03b1b1
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-04 14:10:33 +0330

    add [Authorize] attribute for all methods of TicketController.cs

--- Ticketing/ApiLayer/Controllers/TicketController.cs
+++ Ticketing/ApiLayer/Controllers/TicketController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Ticketing.businessLogicLayer.Services.Interfaces;
 using Ticketing.Dtos.TicketDtos;
@@ -13,6 +14,8 @@ public class TicketController: ControllerBase
     {
         _ticketService = ticketService;
     }
+
+    [Authorize]
     [HttpPost]
     public async Task<IActionResult> CreateTicket(TicketInputDto ticketInputDto)
     {
@@ -20,28 +23,35 @@ public class TicketController: ControllerBase
         return Ok(await _ticketService.CreateTicket(ticketInputDto));
     }
 
+    [Authorize]
     [HttpGet("{id:int}")]
     public async Task<IActionResult> GetTicketById(int id)
     {
         return Ok(await _ticketService.GetTicketById(id));
     }
 
+    [Authorize]
     [HttpGet]
     public async Task<IActionResult> GetAllTickets()
     {
         return Ok(await _ticketService.GetAllTickets());
     }
+
+    [Authorize]
     [HttpGet("GetTicketsByCreatorId/creatorId")]
     public async Task<IActionResult> GetTicketsByCreatorId(int creatorId)
     {
         return Ok(await _ticketService.GetTicketsByCreatorId(creatorId));
     }
+
+    [Authorize]
     [HttpPatch("{id:int}")]
     public async Task<IActionResult> UpdateTicket(int id, UpdateTicketInputDto updateTicketInputDto)
     {
         return Ok(await _ticketService.UpdateTicket(id, updateTicketInputDto));
     }
 
+    [Authorize]
     [HttpPost("SetRating/{id:int}")]
     public async Task<IActionResult> SetTicketRating(int id, int rating)
     {
@@ -49,12 +59,13 @@ public class TicketController: ControllerBase
         return Ok(await _ticketService.SetTicketRating(creatorId, id , rating));
     }
 
+    [Authorize]
     [HttpDelete("{id:int}")]
     public async Task<IActionResult> DeleteTicket(int id)
     {
         return Ok(await _ticketService.DeleteTicket(id));
     }
-
+    [Authorize]
     [HttpPost("FinishTicket")]
     public async Task<IActionResult> FinishTicket(FinishTicketInputDto finishTicketInputDto)
     {

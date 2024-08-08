dffa91531ef2fc77eee35abac4a726312b0d1ca6
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-08 13:41:40 +0330

    TicketInputDtoValidator validator created and its validations implemented

---
+++ Ticketing/businessLogicLayer/Validators/TicketValidator/TicketInputDtoValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+using Ticketing.Dtos.TicketDtos;
+
+namespace Ticketing.businessLogicLayer.Validators.TicketValidator;
+
+public class TicketInputDtoValidator: AbstractValidator<TicketInputDto>
+{
+    public TicketInputDtoValidator()
+    {
+        RuleFor(t => t.Title)
+            .NotEmpty()
+            .WithMessage("the ticket title shouldn't be empty")
+            .Length(5, 50)
+            .WithMessage("the ticket title Length must be between 5 and 50");
+    }
+}

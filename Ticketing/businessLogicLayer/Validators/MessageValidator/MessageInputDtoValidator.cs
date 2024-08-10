420ed2a25c07d3728f5e0ffafea89ff743b74235
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-10 11:46:04 +0330

    add validation for MessageInputDtoValidator.cs

--- Ticketing/businessLogicLayer/Validators/MessageValidator/MessageInputDtoValidator.cs
+++ Ticketing/businessLogicLayer/Validators/MessageValidator/MessageInputDtoValidator.cs
@@ -8,8 +8,16 @@ public class MessageInputDtoValidator: AbstractValidator<MessageInputDto>
     public MessageInputDtoValidator()
     {
         RuleFor(m => m.Body)
+            .NotNull()
+            .WithMessage("the message body is required")
             .NotEmpty()
             .WithMessage($"the message body shouldn't be empty");
+        RuleFor(m => m.TicketId)
+            .NotNull().WithMessage("ticket Id field is required")
+            .NotEmpty().WithMessage("ticket Id field shouldn't be empty")
+            .GreaterThanOrEqualTo(1).WithMessage("ticket id must be greater that 1");
+
+
 
     }
 }

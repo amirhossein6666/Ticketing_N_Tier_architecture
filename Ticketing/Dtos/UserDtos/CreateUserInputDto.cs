c6e9ed2abd0c725e3618d97241afb77865a136eb
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-10 09:18:18 +0330

    make CreateUserInputDto properties nullable for validation purpose

--- Ticketing/Dtos/UserDtos/CreateUserInputDto.cs
+++ Ticketing/Dtos/UserDtos/CreateUserInputDto.cs
@@ -4,10 +4,10 @@ namespace Ticketing.Dtos.UserDtos;
 
 public class CreateUserInputDto
 {
-    public string Username { get; set; }
+    public string? Username { get; set; }
 
-    public string password { get; set; }
+    public string? password { get; set; }
 
-    public Role Role { get; set; }
+    public Role? Role { get; set; }
 
 }

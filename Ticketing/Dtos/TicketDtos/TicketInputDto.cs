9902a0df846fbd9593c813d1c0da92cb4469535d
amirabdollahi <work.amirhossein.abdollahi@gmail.com>
2024-08-10 09:35:07 +0330

    make title property of TicketInputDto.cs nullable for validation purpose

--- Ticketing/Dtos/TicketDtos/TicketInputDto.cs
+++ Ticketing/Dtos/TicketDtos/TicketInputDto.cs
@@ -2,7 +2,7 @@ namespace Ticketing.Dtos.TicketDtos;
 
 public class TicketInputDto
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
     public int CreatorId { get; set; }
 
